090e77bf0169604f96a4bea5d6d119655152fb1a
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2025-08-07 20:20:15 -0300

    final programa

--- Pratica/escola/escola.py
+++ Pratica/escola/escola.py
@@ -11,48 +11,50 @@ class Alunos:
         }
 
     def atividade(self):
-        # Cria cópia para não alterar a lista original
         lista_temporaria = self.lista_alunos.copy()
 
         aula_ingles = []
         aula_danca = []
-        permitidos = 5
+        aula_fisica = []
+        permitidos_por_aula = 5
         todos_alunos = []
 
-        # Junta todos os alunos em uma lista única
         for alunos in lista_temporaria.values():
             todos_alunos.extend(alunos)
 
-        # Seleciona alunos aleatórios sem repetição
-        selecionados = self.selecionados(todos_alunos, permitidos)
-        random.seed
-
-        # Adiciona os selecionados na aula e remove da lista_temporaria
-        ingles = self.adicionar_alunos(selecionados,aula_ingles,lista_temporaria)
-        danca = self.adicionar_alunos(selecionados,aula_danca,lista_temporaria)
         
+        selecionados_ingles = self.selecionados(todos_alunos, permitidos_por_aula)
+        restantes = [aluno for aluno in todos_alunos if aluno not in selecionados_ingles]
+        selecionados_danca = self.selecionados(restantes, permitidos_por_aula)
+        restantes = [aluno for aluno in todos_alunos if aluno not in selecionados_ingles and aluno not in selecionados_danca]
+        selecionados_fisica = self.selecionados(restantes,permitidos_por_aula)
+        
+
+        ingles = self.adicionar_alunos(selecionados_ingles, aula_ingles, lista_temporaria)
+        danca = self.adicionar_alunos(selecionados_danca, aula_danca, lista_temporaria)
+        fisica = self.adicionar_alunos(selecionados_fisica, aula_fisica,lista_temporaria)
+
         print("Alunos na aula de Inglês:", ingles)
-        print("Alunos na aula de Inglês:",danca )
+        print("Alunos na aula de Dança:", danca)
+        print("Alunos na aula de Dança:", fisica)
+
         print("Lista temporária após remoção:")
         print(lista_temporaria)
 
-    def selecionados(self, todos, permit):
-        return random.sample(todos, permit)
+    def selecionados(self, todos, quantidade):
+        return random.sample(todos, quantidade)
 
     def adicionar_alunos(self, selecionados, aula, lista_temp):
-        # Adiciona os selecionados na aula e remove da lista_temporaria
         for aluno in selecionados:
             aula.append(aluno)
             for sala in lista_temp:
                 if aluno in lista_temp[sala]:
                     lista_temp[sala].remove(aluno)
-                    
-        return aula            
+        return aula
 
-        
-# Exemplo de uso:
 turma = Alunos()
 turma.atividade()
+
 
         
       
