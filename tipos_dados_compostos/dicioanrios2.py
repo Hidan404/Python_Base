468a0bcdbb6e71bd7b11078abbbe3cab12dd963a
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2025-08-08 21:45:12 -0300

    create dicioanriso2

---
+++ tipos_dados_compostos/dicioanrios2.py
@@ -0,0 +1,15 @@
+cliente =  {"nome": "Ronald"}
+produto = {
+    "produto": "Caneta",
+    "preco": 5.20
+}
+
+coompra = {
+    "cliente": cliente,
+    "produto": produto,
+    "quantidade": 5
+}
+
+
+
+total = coompra["quantidade"] * produto["preco"]
