83016074f68b72d9ede2728c0edadcc17c21e3c8
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2025-09-03 21:17:26 -0300

    feat: implement bubble sort algorithm

---
+++ tipos_dados_compostos/estoque.py
@@ -0,0 +1,10 @@
+estoque = {
+    "001": {"nome": "Caneta", "quantidade": 100, "preco": 1.50},
+    "002": {"nome": "Caderno", "quantidade": 200, "preco": 5.00},
+    "003": {"nome": "Borracha", "quantidade": 150, "preco": 0.75},
+    "004": {"nome": "Lápis", "quantidade": 300, "preco": 0.50},
+    "005": {"nome": "Mochila", "quantidade": 50, "preco": 120.00},
+}
+
+
+def exibir_estoque(estoque):
