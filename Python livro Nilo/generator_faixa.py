0412d927405d275cb12b20f2586e5e625e10a94f
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2025-09-25 21:12:52 -0300

    implementa função faixa para gerar sequência de números inteiros

--- Python livro Nilo/generator_faixa.py
+++ Python livro Nilo/generator_faixa.py
@@ -1,1 +1,21 @@
-def faixa()
+def faixa(inicio = 0, fim = 10, passo = 1):
+    """Gera uma sequência de números inteiros"""
+    numero = inicio
+    while numero < fim:
+        yield numero
+        numero += passo
+
+print(list(faixa(1)))       
+
+
+def fatorial(n):
+    """Calcula o fatorial de n"""
+    fat = 1
+    for i in faixa(1, n + 1):
+        fat *= i
+        yield fat
+
+
+for numero, valor in enumerate(fatorial(10)):
+    print(f'{numero + 1}! = {valor}')        
+    
