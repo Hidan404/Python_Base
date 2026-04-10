c1027f5d021973a78535035f6f4a2826d0852d62
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2026-04-09 22:20:17 -0300

    treino de logica

---
+++ Exercicios_python_base/maior_num.py
@@ -0,0 +1,15 @@
+'''numeros = [1,190,78,9,189]
+maior = 0
+for i in numeros:
+    if i > maior:
+        maior = i
+
+print(maior)'''
+
+contador = 0
+
+for i in range(1, 5):
+    for j in range(1,4):
+        contador+= 1
+
+    
