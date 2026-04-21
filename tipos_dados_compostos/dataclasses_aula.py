da8eab2d146e4ff4335a3114167b4bf86071198f
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2026-04-21 17:21:06 -0300

    update dataclass.py

--- tipos_dados_compostos/dataclasses_aula.py
+++ tipos_dados_compostos/dataclasses_aula.py
@@ -1,14 +1,14 @@
 from dataclasses import dataclass
 
 #@dataclass(init=False) tenho que definir meu proprio init da classe
-@dataclass
+@dataclass()
 class Pessoa:
     nome: str
     sobrenome: str
     idade: int
 
-    def __post_init__():
-        print("Depois do init")
+    #def __post_init__():
+        #print("Depois do init")
 
 
     @property
@@ -30,3 +30,9 @@ if __name__ == "__main__":
     print(pessoa1 == pessoa2)   
     pessoa1.nome_completo = "Ronald Antonio vaurz"
     print(pessoa1.nome_completo)
+
+    #sorted() ordena a lista de pessoas, para isso tem que ser definido o order = True no dataclass
+    lista_pessoas = [Pessoa("Hidan","Jashin", 21), Pessoa("kakuzu", "tataro",98), Pessoa("Itachi","Uchiha", 21)]
+    #print(sorted(lista_pessoas))
+
+    print(sorted(lista_pessoas,reverse=True, key=lambda pessoa: pessoa.idade))
