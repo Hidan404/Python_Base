db517e78e8743f7fc18b61b65d791178da54f66b
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2026-06-05 22:00:33 -0300

    aprimorando organizador de arquivos

--- Python_analista_dados/listar_diretorios.py
+++ Python_analista_dados/listar_diretorios.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+from logging import basicConfig, INFO, info
 
 
 def listar_diretorios(caminho):
@@ -16,13 +16,24 @@ caminho = Path(__file__).parent.parent
 
 
 def organizar_arquivos():
+    arquivos_movidos_qtd = 0
+
+
     def limpar_caminho():
         caminho = input("Digite o caminho: ").strip().replace("'","").replace('"',"")
         try:
-            pass
+            path = Path(caminho)
+            if not path.exists():
+                raise FileNotFoundError("Caminho não encontrado.")
+            if not path.is_dir():
+                raise NotADirectoryError("O caminho não é um diretório.")
         except Exception as e:
-            
-    path = Path("/home/hidan/Documentos/")
+            print(f"Erro: {e}")
+            return limpar_caminho()
+        return caminho
+    
+    caminho_corrigido = limpar_caminho()        
+    path = Path(caminho_corrigido)
     for item in path.iterdir():
         if item.is_file():
             extensao = item.suffix.lower().replace(".", "")
@@ -33,9 +44,29 @@ def organizar_arquivos():
             print(nova_pasta)
 
             nova_pasta.mkdir(exist_ok=True)
+
             nova_pasta = nova_pasta / item.name
+            if nova_pasta.exists():
+                print(f"Arquivo {item.name} já existe em {nova_pasta.parent}. Pulando...")
+                continue
             item.rename(nova_pasta)
+            arquivos_movidos_qtd += 1
+
+    print(f"Total de arquivos movidos: {arquivos_movidos_qtd}")        
 
             
 
 organizar_arquivos()    
+
+
+
+import pandas as pd
+
+
+def ler_csv(caminho):
+    try:
+        arquivo_csv = pd.read_csv(caminho)
+        print(arquivo_csv)
+    except FileNotFoundError as f:
+        print(f"Erro: {f}")
+        
