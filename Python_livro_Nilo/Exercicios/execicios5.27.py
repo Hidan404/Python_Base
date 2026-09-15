d06ba14f742085fb46d1d5a49727fce0ce494127
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2026-09-15 08:34:15 -0300

    feat: add palindrome checking functionality to the number input script

--- Python_livro_Nilo/Exercicios/execicios5.27.py
+++ Python_livro_Nilo/Exercicios/execicios5.27.py
@@ -1,2 +1,13 @@
 def numero_palindromo():
     numero = input("Digite: ")
+    numero_reverso = ""
+
+    for i in numero[::-1]:
+        numero_reverso+= i
+    if numero_reverso == numero:
+        print(f"Numero é palindromo {int(numero)} | {int(numero_reverso)}")   
+    else:
+        print(f"Numero não é palindromo {int(numero)} | {int(numero_reverso)}")    
+
+
+numero_palindromo()
