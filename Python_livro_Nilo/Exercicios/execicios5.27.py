ecc48bfc460ca952742e9404c7780e64c81ae290
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2026-09-15 22:21:37 -0300

    feat: add function to check if a number is a palindrome using reversed method

--- Python_livro_Nilo/Exercicios/execicios5.27.py
+++ Python_livro_Nilo/Exercicios/execicios5.27.py
@@ -9,5 +9,14 @@ def numero_palindromo():
     else:
         print(f"Numero não é palindromo {int(numero)} | {int(numero_reverso)}")    
 
+def numero_reverso2():
+    numero = input("Digite: ")
+    invertido = "".join(reversed(numero))
+    if invertido == numero:
+        print(f"Numero é palindromo {int(numero)} | {int(invertido)}") 
+    else:
+        print(f"Numero não é palindromo {int(numero)} | {int(invertido):>15}")    
 
-numero_palindromo()
+
+#numero_palindromo()
+numero_reverso2()
